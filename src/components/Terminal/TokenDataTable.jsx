3d3b5f5ed782c093b2e153c7eb6ab091c0f70f1c
Nikita O'Keeffe <okeeffe.nikita@gmail.com>
2024-06-13 20:15:50 +1000

    lp feed a little bigger

--- src/components/Terminal/TokenDataTable.jsx
+++ src/components/Terminal/TokenDataTable.jsx
@@ -28,7 +28,7 @@ function TokenDataTable() {
 
   const tableStyles = {
     container:
-      "text-green-500 flex flex-row px-0 py-0 border-t lg:border-b lg:border lg:mt-2 border-green-900 lg:bg-green-900/5 w-full h-[52vh] lg:h-[80vh] 2xl:h-[60vh] overflow-x-auto lg:tracking-wider",
+      "text-green-500 flex flex-row px-0 py-0 border-t lg:border-b lg:border lg:mt-2 border-green-900 lg:bg-green-900/5 w-full h-[50vh] lg:h-[80vh] 2xl:h-[60vh] overflow-x-auto lg:tracking-wider",
     tableWrapper:
       "w-full h-full scrollbar-thin scrollbar-thumb-transparent scrollbar-track-transparent overflow-y-auto smooth-scroll pr-1 lg:pr-0",
     th: "px-1.5 lg:px-3 py-2 lg:py-3 text-green-500 uppercase tracking-wide md:tracking-wider whitespace-nowrap font-bold",
