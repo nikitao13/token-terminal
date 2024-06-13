c6db95f6fbef5604f280059638175c61500392f5
Nikita O'Keeffe <okeeffe.nikita@gmail.com>
2024-06-13 17:12:23 +1000

    vite node_env config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  if (mode === "development") {
+    process.env.NODE_ENV = "development";
+  } else if (mode === "production") {
+    process.env.NODE_ENV = "production";
+  }
+
+  return {
+    plugins: [react()]
+  };
+});
